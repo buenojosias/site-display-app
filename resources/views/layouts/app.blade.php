f29fc9248409d310b61d8d2feec8a2588a72f381
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2022-08-04 20:26:53 -0300

    Adicionado WireUI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         @livewireStyles
 
         <!-- Scripts -->
+        <wireui:scripts />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
