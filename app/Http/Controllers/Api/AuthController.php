ca664b68c7ae5782fe1e221197851110b628702c
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2022-08-24 22:29:10 -0300

    Api de autenticação

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "email" =>  "required|email",
+            "password" =>  "required",
+        ]);
+        if($validator->fails()) {
+            return response()->json(["validation_errors" => $validator->errors()]);
+        }
+        $user = User::where("email", $request->email)->where("type", "DRIVER")->first();
+        if(is_null($user)) {
+            return response()->json(["status" => "failed", "message" => "E-mail não encontrado"]);
+        }
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            return response()->json(["status" => "success", "login" => true, "token" => $token, "data" => $user]);
+        }
+        else {
+            return response()->json(["status" => "failed", "message" => "Senha incorreta"]);
+        }
+    }
+
+    public function logout() {
+        $user = Auth::user();
+        if(!is_null($user)) {
+            $user->tokens()->where('id', $user->currentAccessToken()->id)->delete();
+            return response()->json(["status" => "success"]);
+        } else {
+            return response()->json(["status" => "failed", "message" => "Usuário não encontrado"]);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\Builder;
+use App\Http\Controllers\Api\{
+    AuthController,
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,11 @@ use Illuminate\Database\Eloquent\Builder;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('auth')->group(function(){
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -36,20 +44,20 @@ Route::get('segments', function(Request $request){
         ->get();
 })->name('api.segments');
 
-Route::get('users', function(Request $request){
-    return \App\Models\User::query()
-        ->select('id','name')
-        ->orderBy('name','asc')
-        ->where('type', 'COMPANY')
-        ->when(
-            $request->search,
-            fn (Builder $query) => $query
-                ->where('name', 'like', "%{$request->search}%")
-            )
-        ->when(
-            $request->exists('selected'),
-            fn (Builder $query) => $query->whereIn('id', $request->input('selected', [])),
-            fn (Builder $query) => $query->limit(10)
-        )
-        ->get();
-})->name('api.users');
+// Route::get('users', function(Request $request){
+//     return \App\Models\User::query()
+//         ->select('id','name')
+//         ->orderBy('name','asc')
+//         ->where('type', 'COMPANY')
+//         ->when(
+//             $request->search,
+//             fn (Builder $query) => $query
+//                 ->where('name', 'like', "%{$request->search}%")
+//             )
+//         ->when(
+//             $request->exists('selected'),
+//             fn (Builder $query) => $query->whereIn('id', $request->input('selected', [])),
+//             fn (Builder $query) => $query->limit(10)
+//         )
+//         ->get();
+// })->name('api.users');
